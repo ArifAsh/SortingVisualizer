d2fd50a78245deac69fed8c1bb20b0edf5b78033
Arif Ashogbon <Arif@Arifs-MacBook-Air.local>
2020-07-23 17:08:46 -0400

    deploying react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import Sorter from './sortingVisualizer.jsx';
 import './App.css';
+import "./sortingVis.css";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sorter></Sorter>
     </div>
   );
 }
