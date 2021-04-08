8f3eaab8e4bcaa7b62a3bcafe3188c3b04774c35
ArifAsh <64379193+ArifAsh@users.noreply.github.com>
2021-04-07 20:12:13 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Sorter from './sortingVisualizer.jsx';
+import './App.css';
+import "./sortingVis.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Sorter></Sorter>
+    </div>
+  );
+}
+
+export default App;
